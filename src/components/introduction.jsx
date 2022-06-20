a25b78b7e4267bde97e9b898b051eeefe9392a2c
vichiuri <chiuri.vic@gmail.com>
2022-06-20 15:25:23 +0300

    another fix

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -7,32 +7,32 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage: 'url(images/cheap.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1>Hi! <br />I'm Victor</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/1BtM-NcHeNbi6v4CqLgxjaTZR8LRYJTsFHsiLr4KHGYQ/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <h1 style={{ color: "maroon" }}>Hi! <br />I'm Victor</h1>
+                          <p ><a style={{ color: "maroon" }}className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/1BtM-NcHeNbi6v4CqLgxjaTZR8LRYJTsFHsiLr4KHGYQ/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View  or Download CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage: 'url(images/fort.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I love building<br /> THINGS !!</h1>
+                          <h1  style={{ color: "green" }}>I love <br/>building<br /> THINGS !!</h1>
 
 
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/vichiuri" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                          <p><a style={{ color: "green" }} className="btn btn-primary btn-learn" href="https://github.com/vichiuri" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
                     </div>
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -27,10 +27,13 @@ export default class Sidebar extends Component {
 
               
               <ul>
-                <li><a href="https://www.facebook.com/victor.gateru.545/" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
-                <li><a href="https://twitter.com/Victreezy" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
+                
                 <li><a href="https://www.linkedin.com/in/victor-chiuri-375321120/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
                 <li><a href="https://github.com/vichiuri" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+                <li><a href="https://www.facebook.com/victor.gateru.545/" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
+                <li><a href="https://twitter.com/Victreezy" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
+                
+           
               </ul>
             </nav>
             <div className="colorlib-footer">
